43e1d8197141f4d8c0ec4fae72e0b1b69baf2a31
fujimura <me@fujimuradaisuke.com>
2011-08-18 17:45:56 +0900

    scope out some functions

--- lib/fb_graph/rails/utils.rb
+++ lib/fb_graph/rails/utils.rb
@@ -1,18 +1,6 @@
 module FbGraph::Rails
   module Utils
 
-    # parse signed request * without verification *
-    #
-    def parse_signed_request(signed_request)
-      encoded_sig, payload = signed_request.split('.')
-      data = ActiveSupport::JSON.decode base64_url_decode(payload)
-    end
-    def base64_url_decode(str)
-      encoded_str = str.gsub('-', '+').gsub('_', '/')
-      encoded_str += '=' while !(encoded_str.size % 4).zero?
-      Base64.decode64(encoded_str)
-    end
-
     # go outside of iframe by rewriting url of frame with JavaScript.
     #
     def relocate_to(url_for_options, options = {})
