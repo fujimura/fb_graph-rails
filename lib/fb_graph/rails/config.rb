00377014c7c000e7ffcc2708b6372d8c8a85551e
Daisuke Goto <idesaku@gmail.com>
2012-01-05 17:46:43 +0900

    Add site_url parameter into config.

--- lib/fb_graph/rails/config.rb
+++ lib/fb_graph/rails/config.rb
@@ -2,7 +2,7 @@ module FbGraph::Rails
   class Config
 
     # Configuration class.
-    # Config.client_id, Config.client_secret, Config.canvas_url can be used.
+    # Config.client_id, Config.client_secret, Config.canvas_url, Config.site_url can be used.
     # Each values will be loaded from /config/facebook.yml or ENV.
     #
     # Sample:
@@ -12,20 +12,23 @@ module FbGraph::Rails
     #     client_id: 115997921798478
     #     client_secret: e98l8888l888888l888888l88888e877
     #     canvas_url: http://apps.facebook.com/fb_graph-rails
+    #     site_url: http://example.com/app
     #   test:
     #     client_id: 115997921798478
     #     client_secret: e98l8888l888888l888888l88888e877
     #     canvas_url: http://apps.facebook.com/fb_graph-rails
+    #     site_url: http://example.com/app
     #   production:
     #     client_id: 115997921798478
     #     client_secret: e98l8888l888888l888888l88888e877
     #     canvas_url: http://apps.facebook.com/fb_graph-rails
+    #     site_url: http://example.com/app
     #
     # ENV:
-    #   fb_client_id, fb_client_secret, fb_canvas_url
+    #   fb_client_id, fb_client_secret, fb_canvas_url, fb_site_url
     #
     class << self
-      [:canvas_url, :client_id, :client_secret].each do |conf|
+      [:canvas_url, :client_id, :client_secret, :site_url].each do |conf|
         define_method conf do
           from_yaml(conf) || from_env(conf)
         end
